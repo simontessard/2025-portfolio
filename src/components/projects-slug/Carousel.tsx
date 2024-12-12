2cf0b64bbbe5e66c4ca552fe0e791ba8d50bfc93
Simon <simon.tessard@gmail.com>
2024-12-12 17:30:23 +0100

    Little fix + lower image quality

--- src/components/projects-slug/Carousel.tsx
+++ src/components/projects-slug/Carousel.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import 'swiper/css';
--- src/components/projects-slug/Heading.tsx
+++ src/components/projects-slug/Heading.tsx
@@ -1,13 +1,15 @@
 export default function Heading({project} : {project: any}) {
     return (
         <div className="mb-8 md:mb-12">
+
             {/* Title */}
             <div className="flex gap-2 font-primary items-start mb-8 md:mb-12">
-                <h1 id="pageTitle"
-                    className="text-4xl md:text-6xl xl:text-8xl uppercase text-primary font-semibold">
+                <h1 className="text-4xl md:text-6xl xl:text-8xl uppercase text-primary font-semibold">
                     {project.title}
                 </h1>
-                <p className="editorial italic text-primary mt-1 xl:mt-2.5">{project.date}</p>
+                <p className="editorial italic text-primary mt-1 xl:mt-2.5">
+                    {project.date}
+                </p>
             </div>
 
             {/* Desc */}
