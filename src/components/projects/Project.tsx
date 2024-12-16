83f282c05831dc5e215ae9b1f050e2948c25a056
Simon <simon.tessard@gmail.com>
2024-12-16 09:56:34 +0100

    Fix prod

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Projet({ title, description, cover, index }) {
+export default function Projet({ title, cover, index } : { title: string, cover: string, index: number }) {
     return (
         <Link scroll={false} href={`/projects/${index}`} className="gsap-project group text-black">
             <div className="relative aspect-square mb-3">
