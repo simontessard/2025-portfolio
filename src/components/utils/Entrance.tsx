3a125d35a7383821c7480cb164283992e91ce261
Simon <simon.tessard@gmail.com>
2024-12-31 11:38:44 +0100

    Changement mots

--- src/components/utils/Entrance.tsx
+++ src/components/utils/Entrance.tsx
@@ -78,21 +78,22 @@ export default function Entrance() {
     return (
         <div className="js-entrance fixed z-[101] flex justify-center items-center h-[100dvh] md:h-screen w-full bg-primary"
         style={{clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}}>
-            <div className="js-entrance--text relative text-white flex items-center gap-3">
 
-                <div className="relative font-primary overflow-hidden text-2xl md:text-3xl uppercase p-1">
-                    <p className="js-up--first absolute">Interface</p>
+            <div className="js-entrance--text relative text-white flex items-end gap-3">
+
+                <div className="relative font-primary overflow-hidden text-right text-2xl md:text-3xl uppercase">
+                    <p className="js-up--first absolute right-0">Création</p>
                     <p className="opacity-0">Interface</p>
-                    <p className="js-up--first absolute">Creation</p>
+                    <p className="js-up--first absolute right-0">Design</p>
                     <p className="js-up--first translate-y-full absolute">Interface</p>
                 </div>
 
-                <div className="relative font-secondary overflow-hidden text-[1.45rem] md:text-[1.75rem] uppercase h-7 md:h-8 md:leading-8 p-1">
-                    <p className="js-up--second absolute max-md:top-0">Responsive</p>
-                    <p className="opacity-0">Responsive</p>
-                    <p className="js-up--second absolute max-md:-translate-y-1">Contenu</p>
-                    <p className="js-up--second absolute translate-y-8 md:translate-y-full">Creatif</p>
+                <div className="overflow-hidden flex flex-col font-secondary text-lg md:text-xl uppercase h-7 md:h-8 w-32 md:leading-8 p-1 max-md:mb-0.5">
+                    <p className="js-up--second">unique</p>
+                    <p className="js-up--second">soigné</p>
+                    <p className="js-up--second">moderne</p>
                 </div>
+
             </div>
         </div>
     )
