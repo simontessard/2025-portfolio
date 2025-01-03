c6e2b692c709094d6e65778dcd707b4ed095b879
Simon <simon.tessard@gmail.com>
2025-01-03 15:23:50 +0100

    Tentative fix nom onglet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-      <Html lang="en" title="Portfolio Simon">
+      <Html lang="en">
+          <Head>
+              <title>Simon TESSARD</title>
+          </Head>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
           <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
           <Head/>
