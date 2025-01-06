51a29eb9f219f8f612cc54666a4d1731edadf2eb
Simon <simon.tessard@gmail.com>
2025-01-06 22:26:45 +0100

    Logique déplacé dans le composant

--- src/components/global/Header/Menu.tsx
+++ src/components/global/Header/Menu.tsx
@@ -3,7 +3,6 @@ import {gsap} from "gsap";
 import {MenuCloseButton} from "@/components/global/Header/MenuOpenButton";
 import MenuLink from "@/components/global/Header/MenuLink";
 import Link from "next/link";
-import {hideMenu} from './Menu/menu'
 import {useGSAP} from "@gsap/react";
 
 export default function Menu() {
@@ -27,10 +26,10 @@ export default function Menu() {
 
                 <div className="mt-auto">
                     <div className="js-menu-links -translate-y-1 opacity-0 flex max-lg:flex-col flex-wrap lg:items-center lg:max-w-xl xl:max-w-4xl gap-y-2 xl:gap-y-4 gap-x-6 md:gap-x-7 xl:gap-x-8 self-end w-full mb-12 md:mb-20">
-                        <MenuLink href="/" text="accueil" hideMenu={hideMenu}/>
+                        <MenuLink href="/" text="accueil"/>
                         <span className="hidden lg:block size-3 md:size-4 xl:size-4 bg-white rounded-full"/>
-                        <MenuLink href="/projects" text="projets" hideMenu={hideMenu}/>
-                        <MenuLink href="/about" text="à propos" hideMenu={hideMenu}/>
+                        <MenuLink href="/projects" text="projets"/>
+                        <MenuLink href="/about" text="à propos"/>
                     </div>
                     <div className="js-menu-contact -translate-y-1 opacity-0 flex gap-5 text-white font-primary font-medium uppercase w-full">
                         <Link scroll={false} href="https://www.linkedin.com/in/simon-tessard-138733198/" target="_blank">
--- src/components/global/Header/MenuLink.tsx
+++ src/components/global/Header/MenuLink.tsx
@@ -1,10 +1,20 @@
 import {usePathname} from "next/navigation";
+import {hideMenu, closeMenuFunction} from './Menu/menu'
 import Link from "next/link";
 
-export default function MenuLink({href, text, hideMenu} : {href: string, text: string, hideMenu: () => void}) {
+export default function MenuLink({href, text} : {href: string, text: string}) {
     const pathname = usePathname();
+
+    const handleClick = () => {
+        if (pathname !== href) {
+            hideMenu();
+        } else {
+            closeMenuFunction()
+        }
+    };
+
     return (
-        <Link onClick={hideMenu} href={href} className={`relative group overflow-hidden text-white tracking-tight font-primary py-2 uppercase text-4xl md:text-5xl xl:text-7xl size-fit`}>
+        <Link onClick={handleClick} href={href} className={`relative group overflow-hidden text-white tracking-tight font-primary py-2 uppercase text-4xl md:text-5xl xl:text-7xl size-fit`}>
             {text}
             {pathname === href &&
                 <span className="absolute top-0 bottom-0 my-auto bg-white block h-1 xl:h-1.5 w-full"/>
