95de348e1b5a0b53c0b59a185f02e6cca7aa6838
Simon <simon.tessard@gmail.com>
2025-01-07 23:21:00 +0100

    Hover effect

--- src/components/global/Header/MenuOpenButton.tsx
+++ src/components/global/Header/MenuOpenButton.tsx
@@ -2,16 +2,20 @@ import { openMenuFunction, closeMenuFunction } from './Menu/menu';
 
 export default function MenuOpenButton() {
     return (
-        <button onClick={openMenuFunction} className="no-image fixed z-50 top-4 right-3.5 md:top-6 md:right-6 size-fit uppercase md:text-lg md:leading-4 tracking-tight font-primary text-primary">
-            Menu
+        <button onClick={openMenuFunction} className="group no-image overflow-hidden fixed z-50 top-4 right-3.5 md:top-6 md:right-6 size-fit uppercase md:text-lg md:leading-4 tracking-tight font-primary text-primary">
+            <span className="relative">
+                Menu
+                <span className="absolute top-0 bottom-0 -left-full my-auto bg-primary block h-0.5 w-full md:group-hover:translate-x-full transition-transform duration-300"/>
+            </span>
         </button>
     )
 }
 
 export function MenuCloseButton() {
     return (
-        <button onClick={closeMenuFunction} className="size-fit uppercase md:text-lg md:leading-4 tracking-tight font-primary text-white">
-            Fermer
+        <button onClick={closeMenuFunction} className="relative group overflow-hidden size-fit uppercase md:text-lg md:leading-4 tracking-tight font-primary text-white">
+             Fermer
+             <span className="absolute top-0 bottom-0 -left-full my-auto bg-white block h-0.5 w-full md:group-hover:translate-x-full transition-transform duration-300"/>
         </button>
     )
 }
