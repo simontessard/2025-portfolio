3bafc566c8555039c86bdc7cfcab25cf22e18ba4
Simon <simon.tessard@gmail.com>
2025-01-12 16:20:17 +0100

    Hover animation

--- src/components/home/Contact.tsx
+++ src/components/home/Contact.tsx
@@ -3,13 +3,25 @@ import Link from "next/link";
 export default function Contact() {
     return (
         <div className="flex gap-6 max-lg:ml-auto text-primary font-primary font-medium uppercase xl:translate-y-1.5">
-            <Link scroll={false} href="https://www.linkedin.com/in/simon-tessard-138733198/"
-                  target="_blank">
-                LinkedIn
-            </Link>
-            <Link scroll={false} href="mailto:pro@simontessard.fr" target="_blank">
-                Email
-            </Link>
+            <LinkHover text="LinkedIn" href="https://www.linkedin.com/in/simon-tessard-138733198/"/>
+            <LinkHover text="Email" href="mailto:pro@simontessard.fr"/>
         </div>
+    )
+}
+
+interface LinkHoverProps {
+    text: string;
+    href: string
+}
+function LinkHover({text, href} : LinkHoverProps) {
+    return (
+        <Link className="relative group overflow-hidden" scroll={false} href={href} target="_blank">
+            <span className="block md:group-hover:-translate-y-full transition-transform ease-out duration-300">
+                {text}
+            </span>
+            <span className="block absolute left-0 top-0 translate-y-full md:group-hover:-translate-y-0 transition-transform ease-out duration-300">
+                {text}
+            </span>
+        </Link>
     )
 }
