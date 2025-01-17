2d8fb9893697c84852af0e838f9e3a7358183f95
Simon <simon.tessard@gmail.com>
2025-01-17 21:43:01 +0100

    Nouveau contenu

--- src/pages/work/chefdechantier/index.tsx
+++ src/pages/work/chefdechantier/index.tsx
@@ -12,6 +12,7 @@ export default function Project() {
     const project = projects[2];
 
     const mainImg = project.gallery.large[0];
+    const galleryDesktop = [project.gallery.large[1], project.gallery.large[3], project.gallery.large[2]]
     const galleryFour = [project.gallery.square[2], project.gallery.square[1], project.gallery.square[4], project.gallery.square[3]]
 
     return (
@@ -25,6 +26,7 @@ export default function Project() {
             <div className="pt-24 md:pt-32 pb-12 px-3.5 md:px-6 ">
                 <Heading project={project}/>
                 <MainImage img={mainImg}/>
+                <GalleryDesktop gallery={galleryDesktop} title="Création d'un tunnel" desc="Le but de ce tunnel, connecté via un sous-domaine, est de récupérer de façon automatisé et précise les demandes de devis. L'entreprise et le client recoivent un mail généré de toutes les informations remplies dans les différentes étapes du formulaire."/>
                 <FourImages gallery={galleryFour}/>
             </div>
 
