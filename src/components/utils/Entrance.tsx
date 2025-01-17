d27c08a12ddb78f85fbb0a41a025ccb124aba20a
Simon <simon.tessard@gmail.com>
2025-01-17 20:58:57 +0100

    Ajout de mon blaze

--- src/components/utils/Entrance.tsx
+++ src/components/utils/Entrance.tsx
@@ -18,14 +18,18 @@ export default function Entrance() {
             tl.to(['.js-up--first-1word', '.js-up--second-1word'], {opacity: 0, yPercent: -100, duration: 1, delay: 1.2, ease: 'power4.out',
                 onStart : () => {document.body.style.overflow = "hidden"}
             }, '<')
-            tl.to(['.js-up--first-2word', '.js-up--first-3word', '.js-up--second-2word', '.js-up--second-3word'], {yPercent: -100, duration: 1.2, delay: .2, ease: 'power4.out'}, "<")
+            tl.to(['.js-up--first-2word', '.js-up--first-3word', '.js-up--first-4word', '.js-up--second-2word', '.js-up--second-3word' , '.js-up--second-4word'], {yPercent: -100, duration: 1.2, delay: .2, ease: 'power4.out'}, "<")
 
             // Disparition seconds mots
             tl.to(['.js-up--first-2word', '.js-up--second-2word'], {opacity: 0, yPercent: -200, duration: 1, delay: 1, ease: 'power4.out'})
             tl.to(['.js-up--first-3word', '.js-up--second-3word'], {yPercent: -200, duration: 1.2, delay: .2, ease: 'power4.out'}, "<")
 
             // Disparition troisième mots
-            tl.to(['.js-up--first-3word', '.js-up--second-3word'], {opacity: 0, yPercent: -200, duration: .8, ease: 'power4.out'})
+            tl.to(['.js-up--first-3word', '.js-up--second-3word'], {opacity: 0, yPercent: -300, duration: 1, delay: 1, ease: 'power4.out'})
+            tl.to(['.js-up--first-4word', '.js-up--second-4word'], {yPercent: -300, duration: 1.2, delay: .2, ease: 'power4.out'}, "<")
+
+            // Disparition quatrième mots
+            tl.to(['.js-up--first-4word', '.js-up--second-4word'], {opacity: 0, yPercent: -400, duration: 1, delay: 1, ease: 'power4.out'})
 
             // Disparition du contenu
             tl.to('.js-entrance--text', {autoAlpha: 0, duration: .6, ease: 'power4.out'})
@@ -61,19 +65,21 @@ export default function Entrance() {
         <div className="js-entrance fixed z-[101] flex justify-center items-center h-[100dvh] md:h-screen w-full bg-primary"
         style={{clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"}}>
 
-            <div className="js-entrance--text relative text-white flex items-end gap-1.5 md:gap-3">
+            <div className="js-entrance--text relative text-white flex gap-1.5 md:gap-2">
 
-                <div className="relative overflow-hidden font-primary text-right text-2xl md:text-3xl xl:text-4xl uppercase">
-                    <p className="js-up--first-1word absolute right-0">Créations</p>
-                    <p className="opacity-0">Interfaces</p>
-                    <p className="js-up--first-2word absolute right-0">Designs</p>
-                    <p className="js-up--first-3word translate-y-full absolute">Interfaces</p>
+                <div className="overflow-hidden flex flex-col relative font-primary text-right text-2xl md:text-3xl xl:text-4xl h-7 md:h-8 xl:h-9 uppercase">
+                    <p className="js-up--first-1word">Simon</p>
+                    <p className="js-up--first-2word">Créations</p>
+                    <p className="js-up--first-3word">Designs</p>
+                    <p className="js-up--first-4word">Interfaces</p>
                 </div>
 
-                <div className="overflow-hidden flex flex-col font-secondary text-lg md:text-xl xl:text-2xl uppercase h-7 md:h-8 xl:h-[2.10rem] w-32 md:leading-8 p-1 max-md:mb-0.5">
-                    <p className="js-up--second-1word">uniques</p>
-                    <p className="js-up--second-2word">soignés</p>
-                    <p className="js-up--second-3word">modernes</p>
+                <div
+                    className="overflow-hidden flex flex-col pt-1.5 md:pt-2 xl:pt-2.5 font-secondary text-lg md:text-xl xl:text-2xl uppercase h-7 md:h-8 xl:h-[2.10rem] w-32 md:leading-8 p-1 max-md:mb-0.5">
+                    <p className="js-up--second-1word">TESSARD</p>
+                    <p className="js-up--second-2word">uniques</p>
+                    <p className="js-up--second-3word">soignés</p>
+                    <p className="js-up--second-4word">modernes</p>
                 </div>
 
             </div>
