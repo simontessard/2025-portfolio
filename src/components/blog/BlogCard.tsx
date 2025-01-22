b022b7a47f92b1b8464e6d0a0f8a8945fd517d6c
Simon <simon.tessard@gmail.com>
2025-01-22 14:47:08 +0100

    Forme des cartes articles

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -7,32 +7,34 @@ interface BlogCardProps {
 
 const BlogCard = ({ article }: BlogCardProps) => {
     return (
-        <Link href={`/blog/${article.slug}`}
-            className="group flex flex-col bg-primary/5 md:hover:bg-primary/10 transition-colors duration-500"
-        >
-            <Image src={article.cover} width={1000} height={600} loading="eager" alt="Article" className="w-full h-48 md:h-56 2xl:h-64 object-cover"/>
+        <Link href={`/blog/${article.slug}`} className="group flex flex-col">
 
-            <div className="flex flex-col h-full justify-between gap-4 md:gap-5 2xl:gap-6 p-5 md:pt-6">
+            <Image src={article.cover} width={1000} height={600} loading="eager" alt="Article"
+                   className="w-full h-48 md:h-64 2xl:h-72 aspect-square object-cover"/>
 
-                <div className="flex gap-2 items-center font-primary text-primary uppercase max-lg:text-sm">
+            <div className="flex flex-col mt-2 md:mt-3">
+
+                <div className="flex justify-between gap-2 mb-2">
+                    <h3 className="font-primary uppercase text-xl md:text-xl 2xl:text-2xl  text-primary">
+                        {article.title}
+                    </h3>
+                    <svg
+                        className="shrink-0 rotate-90 text-primary size-7 lg:size-8 md:group-hover:-translate-x-1.5 transition-transform duration-300"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 82 82">
+                        <path fillRule="evenodd" clipRule="evenodd"
+                              d="M12.252 41L41 12.252 69.749 41l-4.832 4.832-20.5-20.5v43.002h-6.834V25.331l-20.5 20.5L12.252 41z"
+                              fill="currentColor"/>
+                    </svg>
+                </div>
+
+                <div className="flex gap-2 items-center font-secondary italic text-primary uppercase text-sm">
                     <span>{article.date}</span>
                     <span>•</span>
                     <span>{article.readTime}</span>
                 </div>
 
-                <h3 className="font-primary uppercase text-xl md:text-2xl 2xl:text-3xl  text-primary">
-                    {article.title}
-                </h3>
-
-                <div className="mt-auto flex flex-wrap gap-4 md:gap-5">
-                    {article.categories.map((category: any, index : number) => (
-                        <span key={index} className="text-primary font-secondary italic uppercase text-sm">
-                            {category}
-                        </span>
-                    ))}
-                </div>
-
             </div>
+
         </Link>
     );
 };
--- src/components/blog/BlogGrid.tsx
+++ src/components/blog/BlogGrid.tsx
@@ -5,7 +5,7 @@ const BlogGrid = () => {
     const { articles } = blogData;
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-14 md:gap-10 2xl:gap-12 w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-14 md:gap-10 2xl:gap-y-16 2xl:gap-x-12 w-full">
             {articles.map((article: any) => (
                 <BlogCard key={article.id} article={article} />
             ))}
