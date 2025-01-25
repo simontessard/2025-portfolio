24fbb8025e612b01c2b458aaa9dbb0a399cf57a7
Simon <simon.tessard@gmail.com>
2025-01-25 17:39:40 +0100

    Correctifs

--- src/components/home/HeroHeader.tsx
+++ src/components/home/HeroHeader.tsx
@@ -8,9 +8,9 @@ import NavMobile from "@/components/home/nav/NavMobile";
 export default function HeroHeader() {
     return (
         <section className="h-[100dvh] md:h-screen p-3.5 lg:p-6 overflow-hidden">
-            <div className="max-xl:flex-col-reverse flex size-full">
+            <div className="max-xl:flex-col-reverse flex size-full xl:pt-20">
                 <NavMobile/>
-                <Link href="/work" className="max-xl:h-1/2 size-full xl:w-1/2 xl:pt-20">
+                <Link href="/work" className="max-xl:h-1/2 size-full xl:w-1/2">
                     <Carousel/>
                 </Link>
                 <div className="flex flex-col max-xl:pb-2 max-xl:h-1/2 max-xl:justify-end xl:self-end xl:w-3/5 2xl:w-1/2">
