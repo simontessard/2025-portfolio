d8c0a641e7d53974570010f586d0e1dab22091d0
Simon <simon.tessard@gmail.com>
2025-01-27 17:08:01 +0100

    Better display blog page

--- src/pages/blog/[slug]/index.tsx
+++ src/pages/blog/[slug]/index.tsx
@@ -5,11 +5,13 @@ import articlesContent from '@/data/articlesContent.json';
 import Footer from "@/components/global/Footer/Footer";
 import Curve from "@/components/utils/Curve";
 import Head from "next/head";
+import Image from "next/image";
 
 interface ArticlePageProps {
     article: {
         id: string;
         title: string;
+        cover: string;
         description: string;
         date: string;
         readTime: string;
@@ -60,8 +62,8 @@ export default function ArticlePage({ article, content }: ArticlePageProps) {
 
             <section className="min-h-screen">
 
-                <article className="px-4 pt-20 md:pt-40 pb-16 md:pb-24 max-w-3xl md:max-w-4xl 2xl:max-w-5xl mx-auto prose prose-invert">
-                    <div className="mb-10 md:mb-14">
+                <article className="pt-20 md:pt-40 pb-16 md:pb-24">
+                    <div className="px-3.5 prose prose-invert mx-auto max-w-3xl md:max-w-4xl 2xl:max-w-5xl mb-10 md:mb-14">
                         <div className="flex gap-2 items-center font-primary mb-4">
                             <span className="text-primary/70">{article.date}</span>
                             <span className="text-primary/70">•</span>
@@ -77,7 +79,10 @@ export default function ArticlePage({ article, content }: ArticlePageProps) {
                         </span>
                     </div>
 
-                    <div className="prose prose-h2:uppercase marker:text-black !max-w-none" dangerouslySetInnerHTML={{__html: content}}/>
+                    <Image src={article.cover} alt={article.title} priority width={1920} height={1080} className="h-[30lvh] md:h-[50lvh] 2xl:h-[65lvh] object-cover mb-10 md:mb-14"/>
+
+                    <div className="prose px-3.5 mx-auto !max-w-3xl md:!max-w-4xl 2xl:!max-w-5xl prose-h2:uppercase marker:text-black" dangerouslySetInnerHTML={{__html: content}}/>
+
                 </article>
 
                 <Footer/>
